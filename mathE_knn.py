e5cb83ca364f668ca8fd65d53ed05547e47a8137
MBIAMY NAKMENI HILBERT BORIS <79765207+HILBERTSON-PRO@users.noreply.github.com>
2025-03-05 14:07:55 +0100

    Update mathE_knn.py

--- mathE_knn.py
+++ mathE_knn.py
@@ -15,39 +15,4 @@ left_column,middle,right_column = st.columns(3)
 left_column.button('Visualisation')
 middle.write('Dataset mathE')
 
-df=pd.read_csv("mathE-data.csv",sep=";", encoding='cp1252')
-#suppression de la colonne Keywords
-df = df.drop('Keywords', axis=1)
-df = df.drop('Student ID', axis=1)
-df = df.drop('Question ID', axis=1)
-#prétraitement des données
-#ignorer les notifiation d'erreurs
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
-#transformation en tableau de valeurs
-
-#frequency encoding sur l'attribut student country
-#prétraitement des données
-#application du frequency encoding of student country
-
-freq_country=df['Student Country'].value_counts()
-df['Student Country']=df['Student Country'].map(freq_country/df.shape[0])
-#label encoding de l'attribut Question Level
-for level in range(len(df['Question Level'])):
-    match df['Question Level'][level]:
-        case "Basic":
-            df['Question Level'][level]=0
-        case "Advanced":
-            df['Question Level'][level]=1
-
-#suppression de la colonne keywords
-#df=df.drop("Keywords", axis=1)
-#frequency encoding sur l'attribut 
-freq_topic=df['Topic'].value_counts()
-df['Topic']=df['Topic'].map(freq_topic/df.shape[0])
-
-#frequency encoding de l'attribut Subtopic
-freq_subtopic=df['Subtopic'].value_counts()
-df['Subtopic']=df['Subtopic'].map(freq_subtopic/df.shape[0])
-
 
